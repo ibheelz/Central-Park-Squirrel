55c4da2ef8e3b269644348e96687792bccbb587e
ibheelz <ibheelz@icloud.com>
2024-04-07 22:01:37 +0100

    Pandas Working

--- main.py
+++ main.py
@@ -8,17 +8,19 @@
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
-import pandas
+# import pandas
 
-data = pandas.read_csv("weather_data.csv")
-data_list = data.to_dict()
-temp_data = data["temp"].to_list()
+# data = pandas.read_csv("weather_data.csv")
+# data_list = data.to_dict()
+# temp_data = data["temp"].to_list()
 
-print(temp_data)
+# print(temp_data)
 
 # total = sum(temp_data)
 # average = total / len(temp_data)
 
 # print("The Average is: ", average)
 
-print(data[data.temp == data.temp.max()])
+# print(data[data.temp == data.temp.max()])
+
+import pandas
